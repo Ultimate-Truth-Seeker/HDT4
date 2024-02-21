42a3b7a9fec9a3694c5779c7ff28bba0af33a745
angel <mer23661@uvg.edu.gt>
2024-02-20 21:35:33 -0600

    Clases de listas creadas

---
+++ ListaDoble.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+
+public class ListaDoble<T extends Comparable<T>> implements IList<T> {
+
+   private LinkedList<T> listaDoble = new LinkedList<>();
+
+    @Override
+    public T pop() {
+        return listaDoble.removeLast();
+    }
+
+    @Override
+    public void push(T value) {
+        listaDoble.add(value);
+    }
+
+    @Override
+    public int size() {
+        return listaDoble.size();
+    }
+
+    @Override
+    public void add(T value) {
+        listaDoble.add(value);
+    }
+
+    @Override
+    public T get(int index) {
+        return listaDoble.get(index);
+    }
+}
---
+++ ListaSimple.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class ListaSimple<T extends Comparable<T>> implements IList<T> {
+
+   private ArrayList<T> listaSimple = new ArrayList<>();
+
+    @Override
+    public T pop() {
+        return listaSimple.remove(listaSimple.size() - 1);
+    }
+
+    @Override
+    public void push(T value) {
+        listaSimple.add(value);
+    }
+
+    @Override
+    public int size() {
+        return listaSimple.size();
+    }
+
+    @Override
+    public void add(T value) {
+        listaSimple.add(value);
+    }
+
+    @Override
+    public T get(int index) {
+        return listaSimple.get(index);
+    }
+}
